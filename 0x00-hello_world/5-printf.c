d4ded595b281f66a42e9c5c5391d3cee4438502f
Muller21 <mulukenab95@gmail.com>
2022-09-12 09:08:14 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#incloud <stdio.h>
+
+/**
+ * Description: main - Prints a text
+ * Return: Always 0(Success)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art"\n);
+	return (0);
+}
