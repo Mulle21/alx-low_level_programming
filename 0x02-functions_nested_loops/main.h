aea27e864d42f5365c001acf68f5dd3ad1848f06
Muller21 <mulukenab95@gmail.com>
2022-09-14 12:27:19 -0700

    task 0

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,19 +1,18 @@
-#ifndef HOLBERTON_H_INCLUDED
-#define  HOLBERTON_H_INCLUDED 
-
-int _putchar(char);
+#ifndef MAIN_H
+#define MAIN_H                                                                  /*                                                                              
+* Header file for all prototypes 
+* used on 0x02-functions_nested_loops
+*/
+int _putchar(char c);
 void print_alphabet(void);
 void print_alphabet_x10(void);
 int _islower(int c);
 int _isalpha(int c);
-int print_sign(int c);
-int abs(int);
+int print_sign(int n);
+int _abs(int);
 int print_last_digit(int);
-void jack_bwer(viod);
-void time_table(viod);
-int add(int a, int b);
+void jack_bauer(void);
+void times_table(void);
+int add(int, int);
 void print_to_98(int n);
-void print_time_table(int n);
-void num(int r, int b);
-
 #endif
