6c750168992ff97a6fd93d3f4fc96145900c7fa1
Mulle21 <110990945+Mulle21@users.noreply.github.com>
2022-09-21 15:33:58 -0500

    2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * mul - fuction test if enter is digit
+ *
+ * @a : a is parameter1
+ * @b : b is parameter2
+ * Return: always 0 (sucess)
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+
+}
