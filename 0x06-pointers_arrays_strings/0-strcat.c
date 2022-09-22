6c58f52131aa3dc63ce070304ad588e717a8e15e
Mulle21 <110990945+Mulle21@users.noreply.github.com>
2022-09-22 14:24:29 -0500

    0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -6,26 +6,23 @@
  *
  * Return: void
  */
-
 char *_strcat(char *dest, char *src)
 {
 	int i;
 	int j;
 
-	int = 0;
+	i = 0;
 	while (dest[i] != '\0')
 	{
-	i++;
+		i++;
 	}
 	j = 0;
 	while (src[j] != '\0')
 	{
-
-	dest[i] = src[j];
-	i++;
-	j++;
+		dest[i] = src[j];
+		i++;
+		j++;
 	}
-
 
 	dest[i] = '\0';
 	return (dest);
