411473f4ffcdc20371923c313a7e4379ee012943
Mulle21 <110990945+Mulle21@users.noreply.github.com>
2022-09-23 04:03:29 -0500

    Update 0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,10 +1,10 @@
-#include "main.h"                                                  
+#include "main.h"
 
-/**                                                
- * _strcat - function commute srtings                    
- * @dest: param pointer to a cha                          
- * @src: param pointer to a ch                            
- * Return: return value of det.                           
+/**
+ * *_strcat - function commute srtings
+ * @dest: param pointer to a char
+ * @src: param pointer to a char
+ * Return: return value of dest
  */                                                                                                              
 
 char *_strcat(char *dest, char *src)
