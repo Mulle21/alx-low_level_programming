72fd24bc7b4241fc1d435e7196beb50a87ab1804
Mulle21 <mulukenab95@gmail.com>
2022-09-26 08:34:30 -0700

    7. The line of life is a ragged diagonal between duty and desire

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -4,15 +4,17 @@
  *
  *
  * @a: two dimension array to print
+ * Return: Always 0 (Success)
  */
 void print_chessboard(char (*a)[8])
 {
-	int x = 0, y = 0;
+	int i;
+	int j;
 
-	for (x = 0; x < 8; x++)
+	for (i = 0; x < 8; i++)
 	{
-		for (y = 0; y < 8; y++)
-			_putchar(a[x][y]);
+		for (j = 0; j < 8; j++)
+			_putchar(a[i][j]);
 		_putchar(10);
 	}
 }
