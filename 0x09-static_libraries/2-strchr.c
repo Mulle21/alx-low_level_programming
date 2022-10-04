104b1c603e0f5cf0d796181c5b9182d45abe5ef2
Mulle21 <mulukenab95@gmail.com>
2022-10-04 11:06:30 -0700

    task 0

---
+++ 0x09-static_libraries/2-strchr.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * _strchr - Entry point
+ * @s: input
+ * @c: input
+ * Return: Always 0 (Success)
+ */
+char *_strchr(char *s, char c)
+{
+	for (;; s++)
+	{
+		if (*s == c)
+		{
+			return (s);
+		}
+		if (!*s)
+		{
+			return ('\0');
+		}
+	}
+	return ('\0');
+}
