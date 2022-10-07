5c0c2419676415540b92def8e597d3de54d9e257
Mulle21 <mulukenab95@gmail.com>
2022-10-07 03:36:58 -0700

    100

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,11 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * *_memset - fills memory with a constant byte
+ *
+ *  * @s: memory area to be filled
+ *
+ *   * @b: char to copy
+ *    * @n: number of imes to copy b
+
