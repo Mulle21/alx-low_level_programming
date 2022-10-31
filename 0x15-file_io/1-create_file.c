8c8f953ed0a04a84b7d900ade91bf7d5043f26fa
Mulle21 <mulukenab95@gmail.com>
2022-10-31 12:06:03 -0700

    Under the snow

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -6,30 +6,32 @@
  * create_file - Creates a file.
  * @filename: A pointer to the name of the file to create.
  * @text_content: A pointer to a string to write to the file.
- *
- * Return: If the function fails - -1.
- * 	   Otherwise - 1.
+ * Description: Create a function that creates a file.
+ * Return: 1 on success, -1 on failure
  */
+
 int create_file(const char *filename, char *text_content)
+n = 0;
 {
-	int fd, w, len = 0;
+	int i = 0, file;
 
 	if (filename == NULL)
 		return (-1);
 
-	if (text_content != NULL)
+	if (text_content == NULL)
+		text_content = "";
+
+	while (text_content[i] != '\0')
 	{
-		for (len = 0; text_content[len];)
-			len++;
+		i++;
 	}
 
-	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
-	w = write(fd, text_content, len);
+	file = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 
-	if (fd == -1 || w == -1)
+	if (file == -1)
 		return (-1);
 
-	close(fd);
+	write(file, text_content, i);
 
 	return (1);
 }
