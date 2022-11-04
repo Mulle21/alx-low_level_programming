92e6aba796c7904cbbaf7c11539b00f9c43ad67a
Mulle21 <mulukenab95@gmail.com>
2022-11-04 15:12:19 +0300

    rot13

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -14,6 +14,7 @@ char *rot13(char *s)
 	int j;
 	char data1[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
 	char datarot[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+
 	for (i = 0; s[i] != '\0'; i++)
 	{
 		for (j = 0; j < 52; j++)
@@ -27,4 +28,3 @@ char *rot13(char *s)
 	}
 	return (s);
 }
-
