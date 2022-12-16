80f5872d82b4f06c6ffe76fbcfea348e525bbb6a
Mulle21 <mulukenab95@gmail.com>
2022-12-16 09:53:48 -0800

    task 0

---
+++ 0x18-dynamic_libraries/3-islower.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * _islower - checks for lowercase character
+ * @c: the character to check
+ * Return: 1if c is lowercase, 0otherwise
+ */
+int _islower(int c)
+{
+	return (c >= 'a' && c <= 'z');
+}
